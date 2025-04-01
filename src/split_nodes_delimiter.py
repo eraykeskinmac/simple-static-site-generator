e37cbe2108cec287e9fca52e75d242a106782b29
eraykeskinmac <eraykeskinmac@gmail.com>
2025-04-01 04:11:51 +0300

    Add early return for empty delimiter in split_nodes_delimiter function
    - Updated split_nodes_delimiter function to return the original nodes if the delimiter is not provided, improving handling of edge cases.

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -1,6 +1,9 @@
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    if not delimiter:
+        return old_nodes
+        
     new_nodes = []
     
     for old_node in old_nodes:
